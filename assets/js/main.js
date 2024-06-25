da444a8e1ce33d2bd8ffc014d3c0b5e681ca6c74
Leon2006de <mackleon33@gmail.com>
2024-06-25 09:12:59 +0200

    alles neu
    neu

--- assets/js/main.js
+++ assets/js/main.js
@@ -1,3 +1,25 @@
-function showtest() {
-	alert("Test 1");
+function blau(){
+    window.location.href = "Unterseiten/r6 leon fertig/r6 new.html";
 }
+
+
+
+document.addEventListener('DOMContentLoaded', function() {
+    const events = document.querySelectorAll('.timeline-event');
+
+    const observer = new IntersectionObserver((entries) => {
+        entries.forEach(entry => {
+            if (entry.isIntersecting) {
+                entry.target.classList.add('visible');
+            } else {
+                entry.target.classList.remove('visible');
+            }
+        });
+    }, {
+        threshold: 0.5
+    });
+
+    events.forEach(event => {
+        observer.observe(event);
+    });
+});
